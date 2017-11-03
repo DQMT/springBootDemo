f6bc06cf45b1b5a7d0d38876909008a5566b2d83
dqmt <daoqiemantou@gmail.com>
2017-11-03 11:49:12 +0800

    Update WebConfiguration.java

--- src/main/java/com/example/springBootDemo/WebConfiguration.java
+++ src/main/java/com/example/springBootDemo/WebConfiguration.java
@@ -1,1 +1,9 @@
 
+@Configuration
+public class WebConfiguration {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Bean
+    public RemoteIpFilter remoteIpFilter() {
+        return new RemoteIpFilter();
+    }
